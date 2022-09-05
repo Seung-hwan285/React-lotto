462f9c17c07837667afd43f1866b89ef919900ca
Seung-hwan285 <ghks285@naver.com>
2022-09-05 21:14:55 +0900

    ref : Lotto 함수형 변환

--- src/component/Lotto.js
+++ src/component/Lotto.js
@@ -1,16 +1,32 @@
 import {Component} from "react";
 import "../css/lotto.css"
-export default class Lotto extends Component{
 
-    render() {
 
-        const lotto =this.props.lotto;
+function Lotto({lotto}){
+    return(
 
-        return(
-            <div className="lotto">
-                <span className="lotto-emoji">🎟️</span>
-                <span className="lotto-number">{lotto.join(', ')}</span>
-            </div>
-        )
-    }
+        <div className="lotto">
+
+            <span className="lotto-emoji">🎟️</span>
+            <span className="lotto-number">{lotto.join(', ')}</span>
+        </div>
+    )
 }
+export default Lotto;
+
+
+//
+// export default class Lotto extends Component{
+//
+//     render() {
+//
+//         const lotto =this.props.lotto;
+//
+//         return(
+//             <div className="lotto">
+//                 <span className="lotto-emoji">🎟️</span>
+//                 <span className="lotto-number">{lotto.join(', ')}</span>
+//             </div>
+//         )
+//     }
+// }
